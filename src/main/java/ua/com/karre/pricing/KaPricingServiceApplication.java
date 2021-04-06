779545d7be04ec07ce4e54951df189d76c193402
v-vlasiuk <volodymyr.vlasiuk@gmail.com>
2021-04-06 16:21:21 -0700

    Add Spring Boot with Postgres

---
+++ src/main/java/ua/com/karre/pricing/KaPricingServiceApplication.java
@@ -0,0 +1,13 @@
+package ua.com.karre.pricing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaPricingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KaPricingServiceApplication.class, args);
+    }
+
+}
